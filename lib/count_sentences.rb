56265af07a53be4b8da852815191385fcb137f14
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-06-06 09:23:54 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.split('')[-1] == "."
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.split('')[-1] == "?"
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
-  end
+    if self.split('')[-1] == "!"
+      return true
+    else
+      false
+    end
+end
 
   def count_sentences
-
+    sent_string = self.split(' ')
+    count = 0
+    sent_string. each do |word|
+      if word.end_with?(".") || word.end_with?("?") || word.end_with?("!")
+        count = count + 1
+      end
+    end
+    return count
   end
-end
+end 
