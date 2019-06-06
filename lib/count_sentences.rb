b1f580c30cc67055666c1fc593e9f301cfc1a4d8
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-06 17:35:52 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
 end
+
+def count_sentences
+    counter = 0
+    string_arr = self.split(" ")
+    string_arr.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        counter += 1 
+      end
+    end 
+      counter
+end
+
+end
+
+#x = "This is a string! It has three sentences. Right?"
+#binding.pry
