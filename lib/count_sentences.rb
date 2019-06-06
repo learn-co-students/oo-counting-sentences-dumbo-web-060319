834eb4e24e49acbbd8bc1317962b07ee762b9f96
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-06 13:32:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    counter = self.length 
+    self[counter-1] == "." ? true:false
+
   end
 
   def question?
-
+    counter = self.length 
+    self[counter-1] == "?" ? true: false
+    
   end
 
   def exclamation?
-
+    counter = self.length 
+    self[counter-1] == "!" ? true : false 
   end
 
   def count_sentences
-
+    myArr=[];
+    myArr = self.split(/[!.?] /)
+    return myArr.size
   end
 end
